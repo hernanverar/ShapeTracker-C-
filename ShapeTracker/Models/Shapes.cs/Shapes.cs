83aeaf384acabbbf934be57a1ad8ec1ca9f040e5
hernan verar <hernanverar@gmail.com>
2023-05-02 13:37:50 -0700

    added UserInterface,Banners And Shapes files to our ShapeTracker project

---
+++ ShapeTracker/Models/Shapes.cs/Shapes.cs
@@ -0,0 +1,17 @@
+namespace ShapeTracker.Models
+{
+  public class Rectangle
+  {
+    // Rectangle code here.
+  }
+
+  public class Ellipse
+  {
+    // Ellipse code here.
+  }
+
+  public class Triangle
+  {
+    // Triangle code here.
+  }
+}
